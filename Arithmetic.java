4bee8a3035faf0e9babb923140504072ced663b0
vankatapraveen <vankatapraveen@gmail.com>
2023-03-31 18:22:02 +0530

    Comitting jaa code :praveen

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+public class Arithmetic
+{
+public static void main(String args[])
+{
+int num1=11;
+int num2=12;
+
+int sum=num1+num2;
+
+System.out.println("=======Sum of two  numbers======");
+System.out.println(sum);
+}
+}
